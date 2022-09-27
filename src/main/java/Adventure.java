40f241bfdc13d3e4f52995ddb66a4ad1f64c8ecb
CecilieSJ <ceci749q@stud.kea.dk>
2022-09-27 09:06:19 +0200

    Room: navngivning på vej

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -4,11 +4,16 @@ public class Adventure {
 
     public Adventure() {
 
-        Room room1 = new Room("room1", "hehd");
-        Room room2 = new Room("room2", "ggg");
+        Room room1 = new Room("Cabin", "Something startle");
+        Room room2 = new Room("forest", "It has become dark. The thin air feels like a knife cutting through your skin." +
+                "You have been walking around for hours but it is like the darkness has swallowed you. ");
         Room room3 = new Room("room3", "eeeeg");
         Room room4 = new Room("room4", "uuggg");
         Room room5 = new Room("room5", "ghhhgg");
+        Room room6 = new Room("room6", "ggg");
+        Room room7 = new Room("room7", "jjj");
+        Room room8 = new Room("room8", "fff");
+        Room room9 = new Room("room9", "kkk");
 
         room1.setEast(room2);
         room1.setSouth(room4);
